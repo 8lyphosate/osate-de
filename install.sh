ab567f24a71cb969f45dcbc3a68b1eb6188638d6
8lyphosate <8lyphosate@gmail.com>
2025-01-23 20:33:58 -0800

    rofi fix

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo pacman -S i3 polybar picom rofi alacritty dunst ly mpd ncmpcpp fastfetch bashtop networkmanager ttf-iosevka-nerd ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ueberzug conky feh nitrogen cups
-yay -S spotify clifm mpdris2
+yay -S spotify clifm mpdris2 zscroll-git
 
+mkdir ~/Pictures/
 cp -rf ./src/default-wallpaper.jpg ~/Pictures/
 cp -rf ./src/.config ~/
 cp -rf ./src/.ncmpcpp ~/
-
-nitrogen --set-auto ~/Pictures/default-wallpaper.jpg
 
 systemctl --user enable mpDris2.service
 systemctl --user enable mpd.service
@@ -14,4 +13,6 @@ systemctl enable ly.service
 systemctl enable NetworkManager.service
 systemctl enable cups.service
 
-i3-msg reload
+systemctl start ly.service
+
+nitrogen --set-auto ~/Pictures/default-wallpaper.jpg
